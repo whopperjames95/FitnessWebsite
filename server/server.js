699cc5dd70f5e53c4fe5780a99e95eb5cb6cd553
whopperjames95 <james.goff1030@gmail.com>
2022-08-12 15:46:34 -0700

    back end stuff

--- server/server.js
+++ server/server.js
@@ -4,9 +4,15 @@ const cors = require("cors");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/index.html"))
-})
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../client/index.html"))
+// })
+
+app.use(express.static(path.join(__dirname,"../client")))
+
+
+app.use(express.json());
+app.use(cors());
 
 
 
